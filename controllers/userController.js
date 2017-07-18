02a6d7d8205a91019cc9cc33cfc69063b3d90871
egin10 <egin066@gmail.com>
2017-07-19 01:44:26 +0800

    add updateUserProfile

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,13 +14,30 @@ module.exports = {
         res.render('user/maps'); //maps where am i now
     },
     setting: (req, res, next) => {
-        res.render('user/setting', { user: req.user });
+        res.render('user/setting', { messageError: req.flash('settingMessageError'), messageSuccess: req.flash('settingMessageSuccess') });
     },
     settingPost: (req, res, next) => {
         User.findOne({ 'username': req.user.username }, (err, user) => {
-            if(err) throw err;
+            if (err) throw err;
 
+            if (!user) return req.flash('settingMessageError', `Who are you?`);
 
+            if (!user.validPassword(req.body.oldPassword)) {
+                return req.flash('settingMessageError', `Wrong password`);
+            } else {
+                let newData = {
+                    email: req.body.email,
+                    password: user.generateHash(req.body.newPassword)
+                };
+
+                User.update({ 'username': req.user.username }, //searching on badatase
+                    newData, //newData
+                    err => {
+                        if (err) throw err;
+                        req.flash('settingMessageSuccess', `Your profile has updated`);
+                        res.redirect('/user/setting');
+                    });
+            }
         });
     },
     logout: (req, res, next) => {
